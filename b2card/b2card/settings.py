d156eefa0375cf0e354196a2628a090ab7103e52
moi09 <moi09@LAPTOP-2R6F7IQ4>
2016-10-26 21:20:48 -0200

    ajustes iniciais

--- b2card/b2card/settings.py
+++ b2card/b2card/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'autenticacao.apps.AutenticacaoConfig'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'autenticacao.middleware.AuthenticationGetBasketMiddleware'
 ]
 
 ROOT_URLCONF = 'b2card.urls'
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'b2card.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
