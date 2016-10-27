a0c18381af02a5674da9fc8071b5892b6dc5f4cd
moi09 <moi09@LAPTOP-2R6F7IQ4>
2016-10-27 16:28:55 -0200

    teste

--- b2card/clientes/models.py
+++ b2card/clientes/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Cliente(models.Model):
-    
     cnpj = models.CharField(max_length=19)
     razao_social = models.CharField(max_length=30)
     endereco = models.CharField(max_length=100)
