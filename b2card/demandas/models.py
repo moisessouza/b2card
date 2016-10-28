9c6856b9946dcf5cc547db310b4e1249464c27a0
moi09 <moi09@LAPTOP-2R6F7IQ4>
2016-10-28 09:28:23 -0200

    ajuste tela de login

--- b2card/demandas/models.py
+++ b2card/demandas/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from recursos.models import Funcionario
 from clientes.models import Cliente
+from datetime import datetime  
 
 # Create your models here.
 class Demanda(models.Model):
@@ -8,7 +9,7 @@ class Demanda(models.Model):
     identificacao_cliente = models.CharField(max_length=30)
     descricao_cliente = models.CharField(max_length=30)
     numero_demanda = models.CharField(max_length=30)
-    unidade_administrativa = models.CharField(max_length=30)
+    unidade_administrativa = models.CharField(max_length=30, default=None)
     coordenador = models.ForeignKey(Funcionario, blank=False)
     nomde_documento_resumido = models.CharField(max_length=30)
     informacoes_nfe = models.TextField()
@@ -21,8 +22,8 @@ class Orcamento(models.Model):
 class Propostas(models.Model):
     numero_proposta = models.CharField(max_length=30)
     eh_corrente = models.BooleanField()
-    data_proposta = models.DateField()
-    orcamento = models.ForeignKey(Orcamento, blank = False)
+    data_proposta = models.DateField(default=None)
+    orcamento = models.ForeignKey(Orcamento, blank = False, default=None)
     demanda = models.ForeignKey(Demanda, blank = False)
     
 class Atividades(models.Model):
