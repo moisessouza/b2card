9f7f71ef14f7eafe7c548354fc5c71fd22c5614b
moi09 <moi09@LAPTOP-2R6F7IQ4>
2016-11-15 16:56:47 -0200

    ajustes banco

--- b2card/demandas/migrations/0018_demanda_centro_resultado.py
+++ b2card/demandas/migrations/0018_demanda_centro_resultado.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='demanda',
             name='centro_resultado',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='clientes.CentroResultado'),
+            field=models.ForeignKey(default=None, on_delete=django.db.models.deletion.CASCADE, to='clientes.CentroResultado'),
             preserve_default=False,
         ),
     ]
