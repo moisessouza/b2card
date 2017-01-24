e64045b1b2c9032443804bf21b0e5aea7b947f38
Moises <moi0983@gmail.com>
2017-01-24 12:10:27 -0200

    criacao de modal

--- b2card/static/js/apps/inicial/inicial.js
+++ b2card/static/js/apps/inicial/inicial.js
@@ -1,14 +1,38 @@
 "use strict";
 
-var inicial = angular.module('inicial', ['inicial-services', 'commons', 'ui.bootstrap', 'ui.mask']);
+var inicial = angular.module('inicial', ['inicial-services', 'commons', 'ui.bootstrap', 'ui.mask', 'ngMaterial', 'ngMessages']);
 
-inicial.controller('InicialController', function (InicialService, $scope, $window, $uibModal){
+inicial.controller('InicialController', function (InicialService, $scope, $window, $uibModal, $mdDialog){
 	var $ctrl = this;
 	$ctrl.show=true;
 	
+	$ctrl.abrirmodalalocacao = (ev, atividade) => {
+		
+		var modalInstance = $uibModal.open({
+			animation : $ctrl.animationsEnabled,
+			ariaLabelledBy : 'modal-title',
+			ariaDescribedBy : 'modal-body',
+			templateUrl : '/static/modal/modalAlocacao.html?bust=' + Math.random().toString(36).slice(2),
+			controller : 'ModalAlocacaoController',
+			controllerAs : '$ctrl',
+			//size : 'lg'
+			windowClass: 'app-modal-window',
+			resolve : {
+		    	  atividade: atividade
+			}
+		});
+			
+		modalInstance.result.then(function(data) {
+			configurardemanda(share.demanda.id);
+		}, function() {
+			// $log.info('Modal dismissed at: ' + new Date());
+		});
+		
+	}
 	
-	$ctrl.clientes = InicialService.buscaratividadesprofissional(function (data){
-		$ctrl.clientes = data;
-	});
+	$ctrl.clientes = InicialService.buscaratividadesprofissional();
 	
+}).controller('ModalAlocacaoController', function (atividade, $scope, $window) {
+	var $ctrl = this;
+	$ctrl.atividade = atividade;
 });
