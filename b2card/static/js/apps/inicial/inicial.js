da046d11ae9c9d5a9f001c4811b1e3c9c64fecb2
Moises <moi0983@gmail.com>
2017-01-24 09:16:27 -0200

    pagina principal

--- b2card/static/js/apps/inicial/inicial.js
+++ b2card/static/js/apps/inicial/inicial.js
@@ -4,6 +4,8 @@ var inicial = angular.module('inicial', ['inicial-services', 'commons', 'ui.boot
 
 inicial.controller('InicialController', function (InicialService, $scope, $window, $uibModal){
 	var $ctrl = this;
+	$ctrl.show=true;
+	
 	
 	$ctrl.clientes = InicialService.buscaratividadesprofissional(function (data){
 		$ctrl.clientes = data;
