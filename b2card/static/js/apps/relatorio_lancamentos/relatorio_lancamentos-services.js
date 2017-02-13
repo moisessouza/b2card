4bbcd958ce695278ed60ffb4cb58626568026649
Moises <moi0983@gmail.com>
2017-02-13 10:05:10 -0200

    reajuste em alocacao de horas

--- b2card/static/js/apps/relatorio_lancamentos/relatorio_lancamentos-services.js
+++ b2card/static/js/apps/relatorio_lancamentos/relatorio_lancamentos-services.js
@@ -19,6 +19,10 @@ relatorio_lancamentos.service('RelatorioLancamentosService', function($resource)
 				'set': {method:'POST', isArray: true}
 			});
 			return AlocacaoHoras.set({}, argumentos, callback);
+		},
+		salvaralocacao: function(data, callback) {
+			var AlocacaoHoras =  $resource(BASE_URL + 'relatorio_lancamentos/alocarhoras/');
+			return AlocacaoHoras.save({}, data, callback);
 		}
 	}
 });
