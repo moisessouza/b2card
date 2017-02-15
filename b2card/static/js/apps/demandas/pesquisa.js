b614b4588b83029b5afe36cf5c41bc853ff5cfc6
Moises <moi0983@gmail.com>
2017-02-15 13:18:11 -0200

    navegacao em demandas

--- b2card/static/js/apps/demandas/pesquisa.js
+++ b2card/static/js/apps/demandas/pesquisa.js
@@ -7,6 +7,7 @@ pesquisademanda.controller('PesquisaDemandaController', function (CommonsService
 	$ctrl.show=true;
 	
 	var configurarresultado = function (resultado) {
+		$ctrl.resultado = resultado;
 		if (resultado.demandas) {
 			for(let demanda of resultado.demandas) {
 				demanda.descricao = CommonsService.pad(demanda.id, 5) + ' - ' + demanda.cliente.nome_fantasia + ' - ' + demanda.nome_demanda;
@@ -17,8 +18,38 @@ pesquisademanda.controller('PesquisaDemandaController', function (CommonsService
 	$ctrl.listaclientes = PessoaService.buscarpessoasjuridicas();
 	
 	$ctrl.arguments = {
-		pagina: '1',
+		pagina: 1,
 		ordenar: false
+	}
+	
+	$ctrl.primeirapagina = () => {
+		$ctrl.arguments.pagina = 1;
+		$ctrl.pesquisar();
+	}
+	
+	$ctrl.recuarpagina = () => {
+		let pagina = $ctrl.arguments.pagina;
+		pagina-=1;
+		if (pagina < 1) {
+			pagina = 1;
+		}
+		$ctrl.arguments.pagina = pagina;
+		$ctrl.pesquisar();
+	}
+	
+	$ctrl.avancarpagina = () => {
+		let pagina = $ctrl.arguments.pagina;
+		pagina+=1;
+		if (pagina > $ctrl.resultado.total_paginas) {
+			pagina = $ctrl.resultado.total_paginas;
+		}
+		$ctrl.arguments.pagina = pagina;	
+		$ctrl.pesquisar();
+	}
+	
+	$ctrl.ultimapagina = () => {
+		$ctrl.arguments.pagina = $ctrl.resultado.total_paginas;
+		$ctrl.pesquisar();
 	}
 	
 	$ctrl.resultado = PesquisaDemandaService.buscarcentroresultadoshora($ctrl.arguments, configurarresultado);
@@ -34,7 +65,7 @@ pesquisademanda.controller('PesquisaDemandaController', function (CommonsService
 	
 	$ctrl.pesquisar = () => {
 		$ctrl.arguments.status = $ctrl.status;
-		$ctrl.resultado = PesquisaDemandaService.buscarcentroresultadoshora($ctrl.arguments, configurarresultado);
+		PesquisaDemandaService.buscarcentroresultadoshora($ctrl.arguments, configurarresultado);
 	}
 	
 	$ctrl.abrirdemanda = demanda => {
