89258f6b607e492784f6d6342bf680711e3e7aca
Moises <moi0983@gmail.com>
2017-02-24 12:12:58 -0300

    unidade administrativa em pessoa fisica

--- b2card/cadastros/view_unidadeadministrativa.py
+++ b2card/cadastros/view_unidadeadministrativa.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from cadastros.models import UnidadeAdministrativa
+from cadastros.models import UnidadeAdministrativa, PessoaFisica
 from cadastros import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from utils.utils import converter_string_para_float
+from rest_framework.decorators import api_view
+from cadastros.serializers import UnidadeAdministrativaSerializer
 
 def index(request):
     return render(request, 'unidade_administrativa/index.html');
@@ -28,4 +30,14 @@ class UnidadeAdministrativaDetail(APIView):
         unidade_administrativa = UnidadeAdministrativa.objects.get(pk=unidade_administrativa_id)
         unidade_administrativa.delete()
         serializer = serializers.UnidadeAdministrativaSerializer(unidade_administrativa)
-        return Response(serializer.data)    
+        return Response(serializer.data)
+    
+@api_view(['GET'])
+def buscar_unidade_administrativa_por_pessoa(request, format=None):
+    
+    if request.user.is_superuser:
+        return UnidadeAdministrativaList.get(request, format)
+    else:
+        pessoa_fisica = PessoaFisica.objects.filter(prestador__usuario__id = request.user.id)[0]
+        unidade_administrativas = pessoa_fisica.unidade_administrativas.all().order_by('codigo', 'nome')
+        return Response(UnidadeAdministrativaSerializer(unidade_administrativas, many=True).data)
